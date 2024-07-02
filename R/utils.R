0a0b54545aa1ad0eddbe3aa43a827acc4b1f583b
dgkf <18220321+dgkf@users.noreply.github.com>
2024-07-02 18:39:21 -0400

    chore: refactor; add check for upcoming R release

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,14 @@
 `%||%` <- function(lhs, rhs) {
   if (is.null(lhs)) rhs else lhs
 }
+
+#' Retrieve global available packages filters
+#' 
+#' @noRd
+global_filters <- function() {
+  getOption(
+    "available_packages_filters",
+    # TODO: find alternative that doesn't use internal function, hardcode?
+    utils:::available_packages_filters_default
+  )
+}
