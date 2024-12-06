fe5298ec7809b6990b9c5e699034995f8d4eccc5
Pavel Demin <paveldemin@me.com>
2024-12-06 11:43:47 +0000

    refactor: use `it`

--- tests/testthat/test-PACKAGES.R
+++ tests/testthat/test-PACKAGES.R
@@ -1,23 +1,23 @@
 describe("get_packages", {
-  test_that("fetches the PACKAGES file", {
+  it("fetches the PACKAGES file", {
     packages <- get_packages()
     expect_s3_class(packages, "data.frame")
     expect_gt(nrow(packages), 0)
   })
 
-  test_that("fetches the PACKAGES file for a specific platform", {
+  it("fetches the PACKAGES file for a specific platform", {
     packages <- get_packages(platform = "fedora-38")
     expect_s3_class(packages, "data.frame")
     expect_gt(nrow(packages), 0)
   })
 
-  test_that("fetches the PACKAGES file for a specific platform-version combination", {
+  it("fetches the PACKAGES file for a specific platform-version combination", {
     packages <- get_packages(platform = "ubuntu-22.04-s390x", r_version = "4.1")
     expect_s3_class(packages, "data.frame")
     expect_gt(nrow(packages), 0)
   })
 
-  test_that("works with local repository", {
+  it("works with local repository", {
     packages <- get_packages(
       base_url = system.file("repos", package = "pharmapkgs", mustWork = TRUE),
       platform = "ubuntu-22.04",
@@ -27,7 +27,7 @@ describe("get_packages", {
     expect_gt(nrow(packages), 0)
   })
 
-  test_that("only works with certain platforms", {
+  it("only works with certain platforms", {
     expect_error(get_packages(platform = "i-use-arch-btw"))
   })
 })
