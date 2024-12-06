bf4fafad04e94ff3bb5c07bf602109187c6b7661
Pavel Demin <paveldemin@me.com>
2024-12-06 14:33:33 +0000

    chore: move/delete old code

--- R/risk_fields.R
+++ R/risk_fields.R
@@ -1,3 +1,8 @@
+#' @export
+path_to_RISKFIELDS <- function(path) {
+  fs::dir_ls(path, recurse = TRUE, glob = "*/RISKFIELDS")
+}
+
 # risk_fields("/home/rmagno/R/x86_64-pc-linux-gnu-library/4.4/pharmapkgs/repos/ubuntu-22.04/4.4/src/contrib")
 #' @export
 risk_fields <- function(path) {
