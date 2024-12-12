599cff474463c41989593b2a691226dea2858930
Pavel Demin <paveldemin@me.com>
2024-12-12 09:58:19 +0000

    12 incrementally update packages (#13)
    This PR introduces the following changes:
    - Change of logic of how PACKAGES file is updated to ensure an incremental approach
      - old functions in PACKAGES.R are removed and replaced with the new implementation
      - new logic is split into multiple functions with corresponding unit tests
      - new implementation has only 1 dependency - riskmetric itself
    - Add shared .config environment to the package
      -  this object if populated when the package is loaded
      - its values are populated from the environment variables with default fallbacks
      - the idea is that this package will be used in a github matrix and the values will be set there
     - Updates to DESCRIPTION, NAMESPACE and .Rd files
     - Add .lintr configuration file

--- R/PACKAGES.R
+++ R/PACKAGES.R
@@ -1,58 +1,152 @@
-#' Get the PACKAGES file
+#' Read PACKAGES file from a local or remote repository.
 #'
-#' [get_PACKAGES()] retrieves the PACKAGES file from a repository.
+#' The goal of this function is not to produce any significant
+#' side effects, but rather read and return
 #'
-#' @param destpath Destination path.
-#' @param filename The name of the PACKAGES file, defaults to... uh...
-#'   `"PACKAGES"`.
-#' @param repos Base URL of the repository.
-#' @param type Package type: `"source"`, `"mac.binary"`, or `"win.binary"`.
-#' @param contrib_url URL of the contrib sections of the repository.
-#' @param quiet Passed on to `curl::curl_download()`.
+#' @param base_url Base URL of the repository. Can be a local dir path.
+#' @param platform Platform name.
+#' @param r_version R version.
 #'
-#' @returns Path of downloaded file (invisibly).
+#' @return data.frame
 #'
 #' @export
-get_PACKAGES <- function(destpath,
-                         filename = "PACKAGES",
-                         repos = getOption("repos"),
-                         type = getOption("pkgType"),
-                         contrib_url = utils::contrib.url(repos, type),
-                         quiet = FALSE) {
-  # TODO: argument checking.
-  url <- file.path(contrib_url, "PACKAGES")
-  destfile <- file.path(destpath, filename)
-  fs::dir_create(destpath)
-  curl::curl_download(url = url, destfile = destfile, quiet = quiet)
+get_packages <- function(
+    base_url = .config$remote_base,
+    platform = .config$platform,
+    r_version = .config$r_version) {
+  platform <- match.arg(platform, RHUB_REPO_PLATFORMS)
+
+  full_path <- file.path(base_url, platform, r_version) |>
+    utils::contrib.url(type = .get_repos_type(platform)) |>
+    file.path("PACKAGES")
+
+  connection <- .get_connection(full_path)
+
+  on.exit(close(connection))
+
+  read.dcf(connection, all = TRUE) |>
+    as.data.frame()
 }
 
+#' Identify new or newer packages.
+#'
+#' New packages - are the packages that are present in the remote repository
+#' but not in the local repository.
+#'
+#' Newer packages - are the packages that are present in both repositories,
+#' but the version in the remote repository is newer.
+#'
+#' @param remote_packages Data frame with remote packages.
+#' @param local_packages Data frame with local packages.
+#'
+#' @return character()
+#'
 #' @export
-path_to_PACKAGES <- function(path) {
-  fs::dir_ls(path, recurse = TRUE, glob = "*/PACKAGES")
+diff_packages <- function(remote_packages, local_packages) {
+  stopifnot(
+    "Package" %in% names(remote_packages),
+    "Package" %in% names(local_packages),
+    "Version" %in% names(remote_packages),
+    "Version" %in% names(local_packages)
+  )
+
+  combined_packages <- merge(
+    x = remote_packages,
+    y = local_packages,
+    all.x = TRUE,
+    by = "Package",
+    suffixes = c("_remote", "_local")
+  )
+
+  is_new_or_newer <- mapply(
+    utils::compareVersion,
+    combined_packages$Version_remote,
+    combined_packages$Version_local
+  )
+
+  combined_packages[is_new_or_newer > 0, "Package", drop = TRUE]
 }
 
+#' Assess packages using riskmetric.
+#'
+#' @param packages Character vector with package names.
+#' @param limit Maximum number of packages to assess.
+#'
+#' @return data.frame
+#'
 #' @export
-path_to_RISKFIELDS <- function(path) {
-  fs::dir_ls(path, recurse = TRUE, glob = "*/RISKFIELDS")
-}
-
-#' @export
-bundled_repos <- function(repos = NULL) {
-  repos_path <- system.file("repos", package = "pharmapkgs", mustWork = TRUE)
-  repos_path2 <- ifelse(is.null(repos), repos_path, file.path(repos_path, repos))
-  packages_path <- path_to_PACKAGES(repos_path2)
-  repo_path <- gsub("src/contrib/.*", "", packages_path)
-  paste0("file://", repo_path)
-}
-
-#' @export
-bundled_PACKAGES <- function(repos) {
-  repos_path <- system.file("repos", package = "pharmapkgs", mustWork = TRUE)
-
-  if (missing(repos)) {
-    PACKAGESes <- path_to_PACKAGES(repos_path)
-    return(PACKAGESes)
+score_packages <- function(
+    packages,
+    limit = .config$limit,
+    repos = .config$remote_repo) {
+  if (is.na(limit) || is.null(limit) || !is.finite(limit)) {
+    limit <- length(packages)
+  } else {
+    limit <- min(limit, length(packages))
   }
 
-  path_to_PACKAGES(path = file.path(repos_path, repos))
+  package_names <- packages[seq_len(limit)]
+
+  # NOTE: there is a bug in riskmetric::pkg_ref
+  # it doesn't respect repos argument when x is a vector,
+  # so we have to iterate manually.
+  package_refs <- sapply(
+    X = package_names,
+    FUN = riskmetric::pkg_ref,
+    source = "pkg_cran_remote",
+    repos = repos
+  )
+
+  scores <- lapply(package_refs, function(ref) {
+    message("Scoring: ", ref$name, "_", ref$version)
+    assessment <- suppressMessages(riskmetric::pkg_assess(ref))
+
+    # NOTE: side-effect. These assessment files are to be reused
+    # by the `riskreport` package.
+    saveRDS(
+      assessment,
+      file.path(system.file("report", package = "pharmapkgs"), paste0(ref$name, ".rds"))
+    )
+
+    score <- riskmetric::pkg_score(assessment)
+
+    score$Package <- ref$name
+    score$Version <- ref$version
+
+    lapply(score, as.character) |>
+      as.data.frame()
+  })
+
+  Reduce(x = scores, f = function(acc, nxt) {
+    data <- .sync_colnames(acc, nxt)
+    rbind(data[[1]], data[[2]])
+  })
+}
+
+#' Update local PACKAGES info.
+#'
+#' @param old_local_packages Data frame with PACKAGES info currently stored in the repo.
+#' @param new_local_packages Data frame with scored new PACKAGES info.
+#'
+#' @return data.frame
+#'
+#' @export
+update_packages <- function(old_local_packages, new_local_packages) {
+  data <- .sync_colnames(old_local_packages, new_local_packages)
+  old <- data[[1]]
+  new <- data[[2]]
+
+  old_packages <- old[!old$Package %in% new$Package, ]
+  new_packages <- rbind(old_packages, new)
+  new_packages[order(new_packages$Package), ]
+}
+
+#' Save PACKAGES info to the local repository.
+#'
+#' @param packages Data frame with PACKAGES info.
+#' @param path Path where to write new PACKAGES file.
+#'
+#' @export
+write_packages <- function(packages, path = .config$local_packages) {
+  write.dcf(packages, file = path)
 }
---
+++ tests/testthat/test-PACKAGES.R
@@ -0,0 +1,117 @@
+describe("get_packages", {
+  it("fetches the PACKAGES file", {
+    packages <- get_packages()
+    expect_s3_class(packages, "data.frame")
+    expect_gt(nrow(packages), 0)
+  })
+
+  it("fetches the PACKAGES file for a specific platform", {
+    packages <- get_packages(platform = "fedora-38")
+    expect_s3_class(packages, "data.frame")
+    expect_gt(nrow(packages), 0)
+  })
+
+  it("fetches the PACKAGES file for a specific platform-version combination", {
+    packages <- get_packages(platform = "ubuntu-22.04-s390x", r_version = "4.1")
+    expect_s3_class(packages, "data.frame")
+    expect_gt(nrow(packages), 0)
+  })
+
+  it("works with local repository", {
+    packages <- get_packages(
+      base_url = system.file("repos", package = "pharmapkgs", mustWork = TRUE),
+      platform = "ubuntu-22.04",
+      r_version = "4.4"
+    )
+    expect_s3_class(packages, "data.frame")
+    expect_gt(nrow(packages), 0)
+  })
+
+  it("only works with certain platforms", {
+    expect_error(get_packages(platform = "i-use-arch-btw"))
+  })
+})
+
+describe("diff_packages", {
+  it("returns new packages", {
+    remote_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "3.0"))
+    local_packages <- data.frame(Package = c("A", "B"), Version = c("1.0", "2.0"))
+
+    actual_output <- diff_packages(remote_packages, local_packages)
+    expected_output <- c("C")
+
+    expect_equal(actual_output, expected_output)
+  })
+
+  it("returns newer packages", {
+    remote_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "3.0"))
+    local_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "2.5"))
+
+    actual_output <- diff_packages(remote_packages, local_packages)
+    expected_output <- c("C")
+
+    expect_equal(actual_output, expected_output)
+  })
+
+  it("returns empty vector", {
+    remote_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "3.0"))
+    local_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "3.0"))
+
+    actual_output <- diff_packages(remote_packages, local_packages)
+    expected_output <- character()
+
+    expect_equal(length(actual_output), 0)
+    expect_equal(actual_output, expected_output)
+  })
+})
+
+describe("score_packages", {
+  it("returns a data.frame with scores", {
+    packages <- c("rlang")
+
+    actual_output <- score_packages(packages)
+
+    expect_true(all(c("Package", "Version") %in% names(actual_output)))
+    expect_true(any(c("covr_coverage", "size_codebase", "dependencies") %in% names(actual_output)))
+  })
+})
+
+describe("update_packages", {
+  it("returns a data.frame with added new packages", {
+    old_local_packages <- data.frame(Package = c("A", "B"), Version = c("1.0", "2.0"))
+    new_local_packages <- data.frame(Package = c("A", "B", "C"), Version = c("1.0", "2.0", "3.0"))
+
+    actual_output <- update_packages(old_local_packages, new_local_packages)
+
+    expect_equal(nrow(actual_output), 3)
+    expect_equal(actual_output$Package, c("A", "B", "C"))
+    expect_equal(actual_output$Version, c("1.0", "2.0", "3.0"))
+  })
+
+  it("overwrites old local package versions ", {
+    old_local_packages <- data.frame(Package = c("A", "B"), Version = c("1.0", "2.0"))
+    new_local_packages <- data.frame(Package = c("A", "B"), Version = c("1.1", "2.1"))
+
+    actual_output <- update_packages(old_local_packages, new_local_packages)
+
+    expect_equal(nrow(actual_output), 2)
+    expect_equal(actual_output$Package, c("A", "B"))
+    expect_equal(actual_output$Version, c("1.1", "2.1"))
+  })
+
+  it("works with differing fields", {
+    old_local_packages <- data.frame(Package = c("A", "B"), Version = c("1.0", "2.0"))
+    new_local_packages <- data.frame(
+      Package = c("A", "C"),
+      Version = c("1.0", "3.0"),
+      foo = c(1, 3)
+    )
+
+    actual_output <- update_packages(old_local_packages, new_local_packages)
+
+    expect_equal(nrow(actual_output), 3)
+    expect_equal(actual_output$Package, c("A", "B", "C"))
+    expect_equal(actual_output$Version, c("1.0", "2.0", "3.0"))
+    expect_true(all(c("foo") %in% names(actual_output)))
+  })
+})
