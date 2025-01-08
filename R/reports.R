9b9c6b4ae7317f4502c0db6eb969ba5c6741eeb9
Pavel Demin <paveldemin@me.com>
2025-01-08 14:56:22 +0000

    chore: delete localized code

--- R/reports.R
+++ R/reports.R
@@ -1,62 +1,10 @@
-# FIXME: changes required in the riskreports package
-# 1. output_format = "all" for quarto render
-# 2. template_path issue - when using default arg generation fails
-# 3. make changes to the template
-#   - typo in package version
-#   - add gfm to the output format
-# When the problems are fixed, delete this code.
-package_report <- function(
-    package_name,
-    package_version,
-    package = NULL,
-    template_path = system.file("report/pkg_template.qmd", package = "riskreports"),
-    params = list(),
-    ...) {
-
-  full_name <- paste0(package_name, "_v", package_version)
-  output_file <- paste0("validation_report_", full_name, ".html")
-
-  params$package_name <- package_name
-  params$package_version <- package_version
-
-
-  if (is.null(template_path)) {
-    template_path <- system.file("report/pkg_template.qmd",
-      package = "riskreports"
-    )
-  }
-
-  params$assessment_path <- normalizePath(params$assessment_path)
-  # Bug on https://github.com/quarto-dev/quarto-cli/issues/5765
-  quarto::quarto_render(
-    template_path,
-    output_format = "all",
-    execute_params = params,
-    ...
-  )
-
-  lf <- list.files(dirname(template_path), full.names = TRUE)
-  files_template <- lf[!dir.exists(lf)]
-  file_name <- tools::file_path_sans_ext(basename(template_path))
-  files_template <- files_template[startsWith(
-    basename(files_template),
-    file_name
-  )]
-  files_template <- files_template[!endsWith(files_template, ".qmd")]
-  output_file <- paste0(
-    "validation_report_", full_name,
-    ".", tools::file_ext(files_template)
-  )
-  file.rename(files_template, output_file)
-  invisible(output_file)
-}
-
 #' Generate a risk report for a package
 #'
 #' Run riskreport generation, and save the result on disk.
 #'
 #' @param pkg_reference output of `riskmetric::pkg_ref`
 #' @param pkg_assessment output of `riskmetric::pkg_assess`
+#' @param output_dir where to save the report
 #'
 #' @example
 #' pkg_ref <- riskmetric::pkg_ref("ggplot2", source = "pkg_cran_remote")
@@ -64,25 +12,28 @@ package_report <- function(
 #' generate_riskreport(pkg_ref, pkg_assessment)
 #'
 #' @export
-generate_riskreport <- function(pkg_reference, pkg_assessment) {
+generate_riskreport <- function(
+    pkg_reference,
+    pkg_assessment,
+    output_dir = system.file("report", package = "pharmapkgs")) {
   assessment_path <- file.path(
-    system.file("report", package = "pharmapkgs"),
+    output_dir,
     paste0(pkg_reference$name, ".rds")
   )
 
   saveRDS(pkg_assessment, assessment_path)
 
-  # FIXME: use riskreports::package_report
-  outfile <- package_report(
+  outfile <- riskreports::package_report(
     package_name = pkg_reference$name,
     package_version = pkg_reference$version,
-    template_path = system.file("report", "_pkg_template.qmd", package = "pharmapkgs"),
+    # FIXME: still doesn't work with the template file from the package
+    template_path = system.file("report/_pkg_template.qmd", package = "pharmapkgs"),
     params = list(
       assessment_path = assessment_path
     ),
     quiet = TRUE
   )
 
-  file.copy(outfile, file.path(system.file("report", package = "pharmapkgs"), outfile))
+  file.copy(outfile, output_dir)
   file.remove(outfile)
 }
