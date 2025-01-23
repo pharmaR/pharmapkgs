b88cdee6f446890c8c1d2d3c764192e49c45d026
Pavel Demin <paveldemin@me.com>
2025-01-23 13:16:43 +0000

    fix: experiment leftover

--- R/pipeline.R
+++ R/pipeline.R
@@ -4,7 +4,7 @@ github_actions <- function() {
   remote_packages <- get_packages()
   local_packages <- get_packages(base_url = .config$local_base)
   new_packages <- diff_packages(remote_packages, local_packages)
-  scoring_result <- score_packages(new_packages, remote_packages)
+  scoring_result <- score_packages(new_packages)
   scored_packages <- add_score_to_packages(remote_packages, scoring_result$scored_packages)
   new_local_packages <- update_packages(local_packages, scored_packages)
   generate_riskreports(scoring_result$package_refs, scoring_result$package_assessments)
