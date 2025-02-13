788062d35ed3284589393b4c5bffcfd02a19a262
Pavel Demin <paveldemin@me.com>
2025-02-13 20:20:05 +0000

    fix: coerse package version to standard semantics

--- R/PACKAGES.R
+++ R/PACKAGES.R
@@ -3,6 +3,14 @@
 #' The goal of this function is not to produce any significant
 #' side effects, but rather read and return
 #'
+#' Please note, that in order to avoid discrepancies in versioning
+#' semantics, the version field is coerced to `base::package_version`.
+#' For example, `sf` uses 1.0-19, but the majority of packages would
+#' use 1.0.19. `base::package_version("1.0-19")` will return "1.0.19".
+#' Main reason for this is the fact that riskmetric changes the version
+#' of a package from x.y-z to x.y.z, and because of that we can't merge
+#' old existing and new scored packages.
+#'
 #' @param base_url Base URL of the repository. Can be a local dir path.
 #'
 #' @return data.frame
@@ -28,7 +36,11 @@ get_packages <- function(base_url = .config$remote_base) {
   }
 
   tryCatch(
-    as.data.frame(read.dcf(connection, all = TRUE)),
+    expr = {
+      packages <- as.data.frame(read.dcf(connection, all = TRUE))
+      packages$Version <- as.character(package_version(packages$Version))
+      packages
+    },
     error = function(e) {
       warning("Failed to read PACKAGES file: ", e$message)
       data.frame(Package = NA_character_, Version = NA_character_)
