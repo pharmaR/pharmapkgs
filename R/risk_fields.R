60b7b8b51cc463dddc42b8374ecab7471843ec7e
Pavel Demin <paveldemin@me.com>
2025-02-24 14:05:04 +0000

    docs: document old functions

--- R/risk_fields.R
+++ R/risk_fields.R
@@ -1,11 +1,21 @@
-#' @export
-path_to_RISKFIELDS <- function(path) {
+.path_to_riskfields <- function(path) {
   fs::dir_ls(path, recurse = TRUE, glob = "*/RISKFIELDS")
 }
 
-# risk_fields("/home/rmagno/R/x86_64-pc-linux-gnu-library/4.4/pharmapkgs/repos/ubuntu-22.04/4.4/src/contrib")
+#' Get the risk fields from a repository's RISKFIELDS file.
+#'
+#' @param path Path to the repository.
+#'
+#' @examples
+#' \dontrun{
+#' risk_fields(
+#'   "/home/rmagno/R/x86_64-pc-linux-gnu-library/4.4/pharmapkgs/repos/ubuntu-22.04/4.4/src/contrib"
+#' )
+#' }
+#' @return character() with the content of the RISKFIELDS file.
+#'
 #' @export
 risk_fields <- function(path) {
   path <- sub("^file://", "", path)
-  readLines(path_to_RISKFIELDS(path))
+  readLines(.path_to_riskfields(path))
 }
